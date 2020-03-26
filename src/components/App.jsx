dba7cd5f53867efcc47b90e34cce6e7a44a99d91
Michael Skeen <mmskeen@gmail.com>
2020-03-26 15:57:28 -0600

    handle complex state in an object

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
+
+  function handleChange(e) {
+    const { name, value } = e.target; // cannot access e inside setFullName b/c synthetic event.
+    setFullName(previous => {
+      return name === "fName"
+        ? { ...previous, fName: value }
+        : { ...previous, lName: value };
+    });
+  }
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          onChange={handleChange}
+          name="fName"
+          placeholder="First Name"
+          value={fullName.fName}
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          placeholder="Last Name"
+          value={fullName.lName}
+        />
         <button>Submit</button>
       </form>
     </div>
